ae247e2cf9442e58af56cbce44b20ebeefc40de7
RaduTek <radutux13@gmail.com>
2022-10-30 14:47:17 +0200

    Added build for Windows script

---
+++ setup/build.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+title Build YTMusicDL for Windows
+
+echo Build YTMusicDL for Windows
+echo Requires pyinstaller
+echo.
+: Checking for pyinstaller
+pyinstaller > nul
+if %errorlevel%==9009 (
+    echo pyinstaller is not installed!
+    pause
+    exit /b
+)
+cls
+
+echo Building YTMusicDL for Windows...
+echo.
+pyinstaller --icon "..\other\YTMusicDL_icon.ico" --onefile "..\ytmusicdl.py"
+
+pause
+exit /b
