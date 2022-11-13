534610da32b9de8067d89cbf15dc0711e7535251
RaduTek <radutux13@gmail.com>
2022-11-13 22:22:04 +0200

    Removed old check for pyinstaller

--- setup/build.cmd
+++ setup/build.cmd
@@ -2,21 +2,6 @@
 
 title Build YTMusicDL for Windows
 
-echo Build YTMusicDL for Windows
-echo Requires pyinstaller
-echo.
-: Checking for pyinstaller
-pyinstaller > nul
-if %errorlevel%==9009 (
-    echo pyinstaller is not installed!
-    echo To install use: pip install pyinstaller
-    echo And ensure pyinstaller is available to PATH.
-    echo Press any key to exit...
-    pause > nul
-    exit /b
-)
-cls
-
 echo Building YTMusicDL for Windows...
 echo.
 pyinstaller --collect-all "ytmusicapi" --icon "..\other\YTMusicDL_icon.ico" --onefile "..\ytmusicdl.py"
