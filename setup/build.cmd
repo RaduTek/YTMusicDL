bfb58e096bd3ea219752dcecc351ee99266977fb
RaduTek <radutux13@gmail.com>
2022-11-13 22:24:08 +0200

    New pyinstaller check and command

--- setup/build.cmd
+++ setup/build.cmd
@@ -4,7 +4,14 @@ title Build YTMusicDL for Windows
 
 echo Building YTMusicDL for Windows...
 echo.
-pyinstaller --collect-all "ytmusicapi" --icon "..\other\YTMusicDL_icon.ico" --onefile "..\ytmusicdl.py"
+if not exist "..\venv\Scripts\pyinstaller.exe" (
+    echo Pyinstaller does not exist in virtual environment directory.
+    pause > nul
+    echo Press any key to exit...
+    exit /b
+)
+
+..\venv\Scripts\pyinstaller.exe --collect-all "ytmusicapi" --icon "..\other\YTMusicDL_icon.ico" --onefile "..\ytmusicdl.py"
 
 echo Press any key to exit...
 pause > nul
