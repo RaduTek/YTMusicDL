6a28e84b9cf4567f3901d20ee1e2cf29fb44adbf
RaduTek <me@radutek.uk>
2024-12-20 19:00:14 +0200

    Add common utility functions for JSON handling in tests

---
+++ tests/__common.py
@@ -0,0 +1,26 @@
+import os
+import json
+
+out_dir = "tests/results"
+
+
+def set_out_dir(dir: str):
+    try:
+        os.mkdir(dir)
+        out_dir = dir
+    except OSError as error:
+        print(error)
+
+
+def get_json(data):
+    return json.dumps(data, indent=4)
+
+
+def print_json(data):
+    print(get_json(data))
+
+
+def write_json(data, file: str):
+    file = os.path.join(out_dir, file)
+    with open(file, "w") as f:
+        f.write(get_json(data))
