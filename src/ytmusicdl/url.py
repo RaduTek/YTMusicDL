1af6aa170b7b87c6d1eb9966249f053149114557
RaduTek <me@radutek.uk>
2024-12-20 20:17:08 +0200

    Add URL parsing functions for YouTube Music sources and IDs

---
+++ src/ytmusicdl/url.py
@@ -0,0 +1,74 @@
+from urllib.parse import urlparse, parse_qs
+from ytmusicdl.types import Source, UrlType
+
+
+valid_netlocs = [
+    "music.youtube.com",
+    "www.music.youtube.com",
+    "youtube.com",
+    "www.youtube.com",
+    "youtu.be",
+]
+
+
+def parse_id_type(id: str) -> UrlType:
+    """Parse a playlist or album source ID"""
+
+    if id.startswith("OLAK5uy_") or id.startswith("MPRE"):
+        return "album"
+    elif id.startswith("PL"):
+        return "playlist"
+
+    return "watch"
+
+
+def parse_id(id: str) -> Source:
+    """Parse a source ID"""
+    source = Source(id=id)
+
+    source["type"] = parse_id_type(id)
+
+    base_url = "https://music.youtube.com/"
+    match source["type"]:
+        case "watch":
+            source["url"] = f"{base_url}watch?v={id}"
+        case "playlist":
+            source["url"] = f"{base_url}playlist?list={id}"
+        case "album":
+            source["url"] = f"{base_url}browse/{id}"
+
+    return source
+
+
+def parse_url(url: str) -> Source:
+    """Parse a source URL"""
+    source = Source(url=url)
+
+    parsed = urlparse(url)
+    path = parsed.path.split("/")
+    query = parse_qs(parsed.query)
+
+    if parsed.netloc not in valid_netlocs:
+        raise ValueError("Not a YouTube URL")
+
+    if parsed.netloc == "youtu.be":
+        source["id"] = parsed.path.split("/")[-1]
+        source["type"] = "watch"
+    else:
+        match path[1]:
+            case "watch":
+                source["id"] = query["v"][0]
+                source["type"] = "watch"
+            case "playlist":
+                source["id"] = query["list"][0]
+                source["type"] = "playlist"
+                source["subtype"] = parse_id_type(source["id"])
+                if source["subtype"] == "playlist":
+                    source.pop("subtype")
+            case "browse":
+                source["id"] = parsed.path.split("/")[-1]
+                source["type"] = "album"
+            case _:
+                raise ValueError("Invalid YouTube Music URL")
+
+    return source
