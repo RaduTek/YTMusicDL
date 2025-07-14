903fc73385093f24e36aa633ae003a005fa4309e
RaduTek <me@radutek.uk>
2025-07-14 17:36:20 +0300

    Remove unused utils functions

--- src/ytmusicdl/utils.py
+++ src/ytmusicdl/utils.py
@@ -4,6 +4,7 @@ import json
 
 
 def sourceable_str(sourceable: Sourceable) -> str:
+    """Return a string representation of a sourceable object (Song, Playlist, etc.)"""
     return f"'{sourceable['title']}' ({sourceable["id"]})"
 
 
@@ -11,38 +12,6 @@ def song_str(song: Song, config: Config) -> str:
     """Return a string representation of a song"""
     sep = config["artist_separator"]
     return f"{song["title"]} - {sep.join(artist["name"] for artist in song["artists"])}"
-
-
-# Write dict to JSON file
-def write_out_json(my_dict, file_name):
-    s = json.dumps(my_dict, indent=2)
-    with open(file_name, "w") as fo:
-        fo.write(s)
-        fo.close()
-    return s
-
-
-# Sanitize a string to be usable in file names
-def sanitize_filename(filename: str, replace: chr):
-    allowed_chars = " .,!@#$()[]-+=_"
-    new_fn = ""
-    for char in filename:
-        if char.isalnum() or char in allowed_chars:
-            new_fn += char
-        else:
-            new_fn += replace
-    new_fn.strip()
-    if new_fn[-1] == ".":
-        new_fn = new_fn[:-1]
-    return new_fn
-
-
-# Join artist list with defined separator
-def join_artists(artists: list[Artist], separator: str = ", "):
-    artist_names = list()
-    for artist in artists:
-        artist_names.append(artist["name"])
-    return separator.join(artist_names)
 
 
 def length_to_seconds(length: str) -> int:
